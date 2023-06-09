f79648c3a25af8ee2919d33877819d7254fedae2
JoseMmm09 <135858080+JoseMmm09@users.noreply.github.com>
2023-06-09 10:03:55 -0600

    Actualizacion index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+//Ruta para sumar dos numeros.
+app.get('/sumar/:num1/:num2',(req,res) = > {
+const {num1 , num2 } = req.params;
+const resultado = sumar(parseFloat(num1),parseFloat(num2));
+res.json({resultado});
+});
+
+//Puerto en el se ejecutara la API
+const port = process.env.PORT || 3000;
+app.listen(port, () =>{
+console.log('API running on port ${port}');
+});
