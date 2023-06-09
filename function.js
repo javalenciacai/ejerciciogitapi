defeea6f29a588a9151c6b51685c3fa329d7d667
espinoxa <espinoxa@gmail.com>
2023-06-09 11:08:41 -0500

    modify sumar

--- function.js
+++ function.js
@@ -1,4 +1,7 @@
 function sumar(a,b){
+    if(typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error("los argumentos deben ser números");
+    }
     return a+b;
 }
 module.exports = {sumar};
