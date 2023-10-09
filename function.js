0f9e225bb1fd882d48d0380dc2b611b6ab398b11
James Valencia <javalenciacai@gmail.com>
2023-10-09 17:12:29 -0500

    delete all info

--- function.js
+++
@@ -1,8 +0,0 @@
-function sumar(a,b){
-    if(typeof a !== 'number' || typeof b !== 'number') {
-        throw new Error("los argumentos deben ser números");
-    }
-    return a+b;
-}
-
-module.exports = {sumar};
