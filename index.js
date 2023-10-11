eb8c609adfe3a82309735e6b729b230c4f022958
Luis Caballero <cabalugu@outlook.com>
2023-10-11 11:51:04 -0500

    Modificar el archivo index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { sumar } = require('./functions');
 
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.unsubscribe(cors());
+
+app.get('/sumar/:num1/num2', (req, res) => {
+    const { num1, num2 } = req.params;
+    const resultado = sumar(parseFloat(num1), parseFloat(num2));
+    res.json({ resultado });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`API runnig on port ${port}`);
+});
